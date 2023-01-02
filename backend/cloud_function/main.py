07d8f8bbf00c4867b0cf4785c4358d7300cb4ce2
Daniel Owens <danowens878@gmail.com>
2023-01-02 22:27:18 +0000

    Add CORS to cloud function, request to UI

--- backend/cloud_function/main.py
+++ backend/cloud_function/main.py
@@ -8,6 +8,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def request_handler(request):
+    # TODO: Extract CORS handling to wrapper
+
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     publisher = pubsub_v1.PublisherClient()
 
 
@@ -19,4 +39,4 @@ def request_handler(request):
 
     print("Published message")
     
-    return make_response('Message published', 200)
+    return make_response('Message published', 200, headers)
--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
-import { getAuth, signOut } from "firebase/auth";
+import { User, getAuth, signOut } from "firebase/auth";
 import React from "react";
 import styled from "styled-components";
 
 import { useAuth } from "../../auth/useAuth";
 import { Button } from "../Button/Button";
 import { breakpoints } from "../theme";
+import { TextField } from "../TextField/TextField";
 
 const Page = styled.div`
   @media (max-width: ${breakpoints.sm}) {
@@ -21,6 +22,32 @@ const Page = styled.div`
   border-radius: 5px;
 `;
 
+const cloudFunctionUrl =
+  "https://dreambook-request-handler3-rict3hrzrq-ez.a.run.app";
+
+async function submitPrompt(prompt: string, user: User) {
+  const token = await user.getIdToken();
+
+  const response = await fetch(cloudFunctionUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      prompt,
+    }),
+  });
+
+  if (response.status === 200) {
+    console.log("Prompt submitted");
+  } else {
+    console.error("Error submitting prompt");
+  }
+
+  return response.json();
+}
+
 export const Dashboard: React.FC = () => {
   const { user } = useAuth();
 
@@ -28,8 +55,14 @@ export const Dashboard: React.FC = () => {
     return null;
   }
 
+  const [prompt, setPrompt] = React.useState<string>("");
+
   return (
     <Page>
+      <TextField value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+      <Button primary={true} onClick={() => submitPrompt(prompt, user)}>
+        Submit prompt
+      </Button>
       <Button primary={false} onClick={() => signOut(getAuth())}>
         Sign out
       </Button>
