2b362920fb699ada8091bfa5f52b579f6f6e6ead
Daniel Owens <danowens878@gmail.com>
2023-02-09 21:26:54 +0100

    Tag release with 'latest'

--- backend/worker/src/common/inference.py
+++ backend/worker/src/common/inference.py
@@ -1,4 +1,4 @@
-from torch import autocast, cuda
+from torch import autocast, cuda, float16
 from PIL import Image
 from diffusers import StableDiffusionPipeline
 from common.config import get_huggingface_cli_password
@@ -10,11 +10,15 @@ assert cuda.is_available(), "CUDA is not available."
 class StableDiffusionInference:
     def __init__(self):
         self.device = "cuda" if cuda.is_available() else "cpu"
+        torch_dtype = (
+            float16  # cpu doesn't support float16; TODO: fix bug with float not working
+        )
 
         huggingface_token = get_huggingface_cli_password()
 
         self.pipeline = StableDiffusionPipeline.from_pretrained(
             "runwayml/stable-diffusion-v1-5",
+            torch_dtype=torch_dtype,
             cache_dir="./cache",
             device_map="auto",  # https://github.com/huggingface/diffusers/issues/968
             use_auth_token=huggingface_token,
